9d94e98bab3ef35c97bc58c662d04b79cab191e1
Oscar Pinto <moka117@hotmail.com>
2019-01-30 08:32:29 -0500

    Update

--- src/main/java/com/Tour4you/Connector/ConnectorImpl/ConnectorTwitter.java
+++ src/main/java/com/Tour4you/Connector/ConnectorImpl/ConnectorTwitter.java
@@ -1,6 +1,29 @@
 package com.Tour4you.Connector.ConnectorImpl;
 
-import com.Tour4you.Connector.Connector;
 
+import com.Tour4you.Connector.Connector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping(ConnectorTwitter.TWITTER_BASE_URI)
 public class ConnectorTwitter implements Connector {
+
+    public static final String TWITTER_BASE_URI= "svc/v1/tweets";
+
+    @Autowired
+    private Twitter twitter;
+
+
+    @RequestMapping(value ="{hashtag", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Tweet> getTweets(@PathVariable final String hashTag){
+        return twitter.searchOperations().search(hashTag,20).getTweets();
+
+    }
 }
---
+++ src/main/java/com/Tour4you/TwitterApp.java
@@ -0,0 +1,13 @@
+package com.Tour4you;
+
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class TwitterApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TwitterApp.class, args);
+    }
+}
